bc8eb47b8933e1736a1f64b79777eb13a6416fe7
Varkey Academy <variablekey@gmail.com>
2015-07-20 00:48:02 +0530

    Testing JavaLessonOne

---
+++ src/javaseries/JavaLessonOne.java
@@ -0,0 +1,18 @@
+package javaseries;
+
+public class JavaLessonOne {
+
+	public static void methodOne(){
+		System.out.println("Namaste ");
+	}
+	
+	public static void methodTwo(){
+		System.out.println("Namaste !!");
+	}
+	
+	public static void main(String[] args) {
+		methodOne();
+		methodTwo();
+	}
+
+}
